0a1b8397b7c265986449e490055c116ce663f9b5
sm-mehedi <hasanmehedi63395@gmail.com>
2024-10-19 19:34:33 +0600

    Nsted components

---
+++ first-react-app/src/Components/Products/Products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Product from '../Product/Product';
+export default function Products(){
+    return(
+        <div>
+        <h3> Hey its my first react</h3>
+        <Product></Product>
+        
+        </div>
+    )
+
+    
+}
